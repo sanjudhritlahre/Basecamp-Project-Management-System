bc9d50413e744896825e196e31741cd899a0b945
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-06 17:49:09 +0530

    Basic Configuration of Express & Also CORS Configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Basic Configuration of Express
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// CORS Configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, Welcome to Basecampy : Project Management Platform!");
