52dc7c2a3d7d17bd487ea103df1e3729f7231e93
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-07 02:35:52 +0530

    Catch Errors Better with Async Handler

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,11 +1,22 @@
 import { ApiResponse } from "../utils/ApiResponse.utils.js";
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
 
-const healthCheck = (req, res) => {
+/* 
+const healthCheck = async (req, res, next) => {
   try {
+    const user = await getUserFromDB();
     res.status(200).json(new ApiResponse(200, "Server is running...🚀"));
   } catch (err) {
     console.error("Server is not running...", err);
+    next(err);
   }
 };
+*/
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { message: "Server is running...🚀" }));
+});
 
 export { healthCheck };
