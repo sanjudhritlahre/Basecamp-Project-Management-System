28209d26fcd403ccc7e8ef50a4b621ea07ff96bb
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-07 01:52:33 +0530

    MongoDB Atlas Connection & Database Url String Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "dotenv/config";
-import app from './app.js';
+import app from "./app.js";
+import connectDB from "./db/index.js";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App Server running at PORT: http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App Server running at PORT: http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo Database Connection Failed!", err);
+    process.exit(1);
+  });
