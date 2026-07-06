da61eb7bee4d130c8bd6f1653be85bac3ef500f3
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-06 16:38:38 +0530

    Dot env files on backend & Node Express Types Installation Packages

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import 'dotenv/config';
+
+const db = process.env.DATABASE_URL;
+
+// TODO: Just Debugg the Code and Test Out!
+console.log("DATABASE:", db);
+
 console.log("Start of the Backend Project!");
