d20769b4e7a78d9120d0e4a135396d052a42decf
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-07 03:08:08 +0530

    Hash the Password with Pre Hooks of Mongoose

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -59,5 +60,11 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
