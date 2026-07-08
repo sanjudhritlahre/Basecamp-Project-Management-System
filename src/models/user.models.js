879d9df08bc91756deeb842e44b28e56a4260fdf
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-08 14:39:13 +0530

    Code for Access and Refresh Token with Built-In Crypto Module

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { randomBytes, createHmac } from "node:crypto";
 
 const userSchema = new Schema(
   {
@@ -69,6 +71,42 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateTempraryToken = function () {
+  const unHashedToken = randomBytes(20).String("hex");
+  const hashedToken = createHmac("sha256").update(unHashedToken).digest("hex");
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 Min
+
+  return { unHashedToken, hashedToken, tokenExpiry };
 };
 
 export const User = mongoose.model("User", userSchema);
